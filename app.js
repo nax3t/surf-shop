fba305bc685c1ec7d8aad1718bf3ba4fee130bd6
Ian Schoonover <6356890+nax3t@users.noreply.github.com>
2018-07-04 12:13:27 -0500

    Add user and index routes

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 const index 	= require('./routes/index');
 const posts 	= require('./routes/posts');
 const reviews = require('./routes/reviews');
-const users 	= require('./routes/users');
 
 const app = express();
 
@@ -27,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/posts', posts);
 app.use('/posts/:id/reviews', reviews);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,54 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
 });
 
+/* GET /register */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+
+/* POST /register */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+
+/* GET /login */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login');
+});
+
+/* POST /login */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login');
+});
+
+/* GET /profile */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+
+/* PUT /profile/:user_id */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/:user_id');
+});
+
+/* GET /forgot */
+router.get('/forgot', (req, res, next) => {
+  res.send('GET /forgot');
+});
+
+/* PUT /forgot */
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT /forgot');
+});
+
+/* GET /reset */
+router.get('/reset/:token', (req, res, next) => {
+  res.send('GET /reset/:token');
+});
+
+/* PUT /reset */
+router.put('/reset/:token', (req, res, next) => {
+  res.send('PUT /reset/:token');
+});
+
 module.exports = router;
