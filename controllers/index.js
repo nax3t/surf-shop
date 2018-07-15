20f595bd296dfcf88286eba893de37daa14d836d
Ian Schoonover <6356890+nax3t@users.noreply.github.com>
2018-07-15 14:25:32 -0500

    Move login and logout logic into index controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
+const passport = require('passport');
 
 module.exports = {
+	// POST /register
 	async postRegister(req, res, next) {
 		const newUser = new User({
 			username: req.body.username,
@@ -10,5 +12,17 @@ module.exports = {
 
 		await User.register(newUser, req.body.password);
 		res.redirect('/');
+	},
+	// POST /login
+	postLogin(req, res, next) {
+		passport.authenticate('local', {
+		  successRedirect: '/',
+		  failureRedirect: '/login' 
+		})(req, res, next);
+	},
+	// GET /logout
+	getLogout(req, res, next) {
+	  req.logout();
+	  res.redirect('/');
 	}
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const { postRegister } = require('../controllers');
+const { postRegister, postLogin, getLogout } = require('../controllers');
 const { errorHandler } = require('../middleware')
 
 /* GET home page. */
@@ -23,16 +22,10 @@ router.get('/login', (req, res, next) => {
 });
 
 /* POST /login */
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login' 
-}));
+router.post('/login', postLogin);
 
 /* GET /logout */
-router.get('/logout', (req, res, next) => {
-  req.logout();
-  res.redirect('/');
-});
+router.get('/logout', getLogout);
 
 /* GET /profile */
 router.get('/profile', (req, res, next) => {
