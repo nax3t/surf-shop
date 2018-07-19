59382cad9c2f9336a5de9e3b5b0eadd0406dc109
Ian Schoonover <6356890+nax3t@users.noreply.github.com>
2018-07-19 17:02:10 -0500

    Add posts - new

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,13 @@
 const Post = require('../models/post');
 
 module.exports = {
+	// Posts Index
 	async getPosts(req, res, next) {
 		let posts = await Post.find({});
 		res.render('posts/index', { posts });
+	},
+	// Posts New
+	newPost(req, res, next) {
+		res.render('posts/new');
 	}
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { errorHandler } = require('../middleware');
-const { getPosts } = require('../controllers/posts');
+const { getPosts, newPost } = require('../controllers/posts');
 
 /* GET posts index /posts */
 router.get('/', errorHandler(getPosts));
 
 /* GET posts new /posts/new */
-router.get('/new', (req, res, next) => {
-  res.send('/posts/new');
-});
+router.get('/new', newPost);
 
 /* POST posts create /posts */
 router.post('/', (req, res, next) => {
