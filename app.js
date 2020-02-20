bc9796276fa28f2d5cabe8e56c6bd64ea589b420
Ian Schoonover <iantskon@gmail.com>
2020-02-20 11:12:21 -0600

    Update theming for posts index

--- app.js
+++ app.js
@@ -101,16 +101,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // // render the error page
-  // res.status(err.status || 500);
-  // res.render('error');
-  console.log(err);
-  req.session.error = err.message;
-  res.redirect('back');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
