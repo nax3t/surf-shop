9e3dc233664e2d25d06e987fe7e5d43cf0d08bbe
Ian Schoonover <learntocodeinfo@gmail.com>
2024-12-26 13:52:33 -0600

    Migrate faker to @faker-js/faker and update seeds.js

--- seeds.js
+++ seeds.js
@@ -1,36 +1,62 @@
-const faker = require('faker');
+const { faker } = require('@faker-js/faker');
 const Post = require('./models/post');
 const cities = require('./cities');
 
 async function seedPosts() {
-	await Post.deleteMany({});
-	for(const i of new Array(600)) {
-		const random1000 = Math.floor(Math.random() * 1000);
-		const random5 = Math.floor(Math.random() * 6);
-		const title = faker.lorem.word();
-		const description = faker.lorem.text();
-		const postData = {
-			title,
-			description,
-			location: `${cities[random1000].city}, ${cities[random1000].state}`,
-			geometry: {
-				type: 'Point',
-				coordinates: [cities[random1000].longitude, cities[random1000].latitude],
-			},
-			price: random1000,
-			avgRating: random5,
-			author: '5bb27cd1f986d278582aa58c',
-			images: [
-				{
-					url: 'https://res.cloudinary.com/devsprout/image/upload/v1561315599/surf-shop/surfboard.jpg'
-				}
-			]
-		}
-		let post = new Post(postData);
-		post.properties.description = `<strong><a href="/posts/${post._id}">${title}</a></strong><p>${post.location}</p><p>${description.substring(0, 20)}...</p>`;
-		post.save();
+	try {
+		await Post.deleteMany({});
+		
+		const posts = Array.from({ length: 600 }, () => {
+			const random1000 = Math.floor(Math.random() * 1000);
+			const random5 = Math.floor(Math.random() * 6);
+			const title = faker.lorem.word();
+			const description = faker.lorem.text();
+			
+			const post = new Post({
+				title,
+				description,
+				location: `${cities[random1000].city}, ${cities[random1000].state}`,
+				geometry: {
+					type: 'Point',
+					coordinates: [cities[random1000].longitude, cities[random1000].latitude],
+				},
+				price: random1000,
+				avgRating: random5,
+				author: '5bb27cd1f986d278582aa58c',
+				images: [
+					{
+						url: 'https://res.cloudinary.com/devsprout/image/upload/v1561315599/surf-shop/surfboard.jpg'
+					}
+				]
+			});
+			
+			post.properties.description = `<strong><a href="/posts/${post._id}">${title}</a></strong><p>${post.location}</p><p>${description.substring(0, 20)}...</p>`;
+			return post.save();
+		});
+		
+		await Promise.all(posts);
+		console.log('600 new posts created');
+	} catch (err) {
+		console.error('Error seeding posts:', err);
 	}
-	console.log('600 new posts created');
 }
 
 module.exports = seedPosts;
+
+// Run the seed function if this script is run directly
+if (require.main === module) {
+	const mongoose = require('mongoose');
+	mongoose.connect('mongodb://localhost:27017/surf-shop')
+	.then(() => {
+		console.log('Connected to MongoDB');
+		return seedPosts();
+	})
+	.then(() => {
+		mongoose.connection.close();
+		console.log('Database connection closed');
+	})
+	.catch(err => {
+		console.error('Error:', err);
+		mongoose.connection.close();
+	});
+}
